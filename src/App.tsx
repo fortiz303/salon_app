f237e1db7fc450b94767d1be8e66c69a9ddace85
fortiz <34146196+fortiz303@users.noreply.github.com>
2025-07-05 05:54:14 -0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import QueueSnip from './QueueSnip';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to QueueSnip!</h1>
+      <QueueSnip />
+    </div>
+  );
+}
+
+export default App;
